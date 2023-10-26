6a85ce570469e89714d36f9bfc5cdd7f8dbf4893
Сергей Никаноров <kapterenko@yandex.ru>
2023-10-26 10:21:05 +0800

    Review_3_2

--- LoadPerson.cpp
+++ LoadPerson.cpp
@@ -6,6 +6,12 @@ struct DBParams {
     DBLogLevel db_log_level;
 };
 
+struct PersonFilter {
+    int min_age;
+    int max_age;
+    string_view name_filter;
+}
+
 DBHandler DBHandlerBuilder(const DBParams& params) {
     DBConnector connector(params.db_allow_exceptions, params.db_log_level);
     if (params.db_name.starts_with("tmp."s)) {
@@ -15,7 +21,7 @@ DBHandler DBHandlerBuilder(const DBParams& params) {
     }
 }
 
-optional<vector<Person>> LoadPersons(const DBParams& db_params, const int min_age, const int max_age, const string_view name_filter) {
+optional<vector<Person>> LoadPersons(const DBParams& db_params, const PersonFilter person_filter) {
     
     DBHandler db = move(DBHandlerBuilder(db_params));
 
@@ -26,8 +32,8 @@ optional<vector<Person>> LoadPersons(const DBParams& db_params, const int min_ag
     ostringstream query_str;
     query_str << "from Persons "s
               << "select Name, Age "s
-              << "where Age between "s << min_age << " and "s << max_age << " "s
-              << "and Name like '%"s << db.Quote(name_filter) << "%'"s;
+              << "where Age between "s << person_filter.min_age << " and "s << person_filter.max_age << " "s
+              << "and Name like '%"s << db.Quote(person_filter.name_filter) << "%'"s;
     DBQuery query(query_str.str());
 
     vector<Person> persons;
